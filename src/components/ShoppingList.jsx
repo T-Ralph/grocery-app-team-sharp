6bf4136c13f41f43cca8ee9bf4034cf8e074f6cc
T-Ralph <olaniyitralph@gmail.com>
2020-09-21 11:35:35 -0600

    Added the missing <main> tag to JSX

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -5,66 +5,68 @@ import React from 'react';
 function ShoppingList() {
     return(
         <>
-            <section className="main-section">
-                <h2>
-                    <i className="fas fa-shopping-cart"></i>
-                    Cart
-                </h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>
-                                Item
-                            </th>
-                            <th>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                Item 1
-                            </td>
-                            <td>
-                                <button className="table-button">Remove</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Item 2
-                            </td>
-                            <td>
-                                <button className="table-button">Remove</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Item 3
-                            </td>
-                            <td>
-                                <button className="table-button">Remove</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Item 4
-                            </td>
-                            <td>
-                                <button className="table-button">Remove</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>
-                                Item
-                            </th>
-                            <th>
-                            </th>
-                        </tr>
-                    </tfoot>
-                </table>
-            </section>
+            <main>
+                <section className="main-section">
+                    <h2>
+                        <i className="fas fa-shopping-cart"></i>
+                        Cart
+                    </h2>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Item
+                                </th>
+                                <th>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Item 1
+                                </td>
+                                <td>
+                                    <button className="table-button">Remove</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Item 2
+                                </td>
+                                <td>
+                                    <button className="table-button">Remove</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Item 3
+                                </td>
+                                <td>
+                                    <button className="table-button">Remove</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Item 4
+                                </td>
+                                <td>
+                                    <button className="table-button">Remove</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>
+                                    Item
+                                </th>
+                                <th>
+                                </th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </section>
+            </main>
         </>
     );
 }
