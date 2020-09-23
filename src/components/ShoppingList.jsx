b4ce07f51571df49603f75b38f32bf4066b6571c
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-23 11:55:21 -0600

    Adds key to each ingredient as they are being generated to fix console error;

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -9,7 +9,7 @@ function ShoppingList() {
     // shoppingList is declared as the shoppingReducer global state
     const shoppingList = useSelector( state => state.shoppingReducer );
     // We loop through our shoppingList and make each ingredient a list item to be displayed in an unordered list in the cart section
-    const currentShoppingList = shoppingList.map( ingredient => <tr><td>{ingredient.ingredient.measure} {ingredient.ingredient.ingredient}</td><td><button className="table-button"><i className="fas fa-trash-alt"></i></button></td></tr> );
+    const currentShoppingList = shoppingList.map( ingredient => <tr key={ingredient.id}><td>{ingredient.ingredient.measure} {ingredient.ingredient.ingredient}</td><td><button className="table-button"><i className="fas fa-trash-alt"></i></button></td></tr> );
 
     return(
         <>
